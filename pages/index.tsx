f77a7dc7c40cea8981179d6c8e56a95444a812b4
Deren Somsanith <derensomsanith@gmail.com>
2022-07-15 13:36:37 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
-const Home = () => {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+import { prisma } from '../db/client';
+
+const Home = (props: any) => {
+	return (
+		<>
+			<h1 className='text-3xl font-bold underline'>Hello World!</h1>
+			<div>{props.questions}</div>
+		</>
+	);
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+	const questions = await prisma.pollQuestion.findMany();
+
+	return {
+		props: {
+			questions: JSON.stringify(questions),
+		},
+	};
+};
